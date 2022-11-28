f4f7fc4e2f9bedebddb5f34342b2d973c859a39a
John Tarleton Moore <tarletonmoore@gmail.com>
2022-11-28 13:53:17 -0500

    commit tree ca73be5898d1f029f238c8f8704c110467f1bd37

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,37 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
 
   def index
+    if params[:user_id]
+    user = User.find(params[:user_id])
+    items = user.items
+    else
     items = Item.all
+    end
     render json: items, include: :user
+  end
+
+
+  def show
+    items = Item.find(params[:id])
+    render json: items, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Item not found" }, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
